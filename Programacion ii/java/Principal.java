331d9706089ad35f743600325c5db39919d5dc5d
Airton <airton.mamanih@unmsm.edu.pe>
2026-09-02 11:47:00 -0500

    Ejecutando con java

--- Programacion ii/java/Principal.java
+++ Programacion ii/java/Principal.java
@@ -1,52 +1,53 @@
 package principal;
 
-/**
- *
- * @author Usuario
- */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        TV tv1=new     TV()     ;
-        TV tv2=new     TV()     ;
-        System.out.println("canal= "+tv1.canal+" | "+tv1.volumen+" | "+tv1.on);
+        TV tv1 = new TV();
+        
+        System.out.println("Estado inicial: canal=" + tv1.canal + " | vol=" + tv1.volumen + " | on=" + tv1.on);
+        
         tv1.encender();
-        tv1.subirCanal();
-        tv1.subirCanal();
-        System.out.println("canal= "+tv1.canal);
-        tv1.bajarCanal();
-        tv1.bajarCanal();
-        tv1.bajarCanal();
-        tv1.bajarCanal();
-        System.out.println("canal= "+tv1.canal);    
-        System.out.println("tarea para explicar con detalle:");
-        int a=2000000000, b=2000000000;
-        int c=a+b;
-        System.out.println("a+b= "+c);
+        tv1.subirCanal(); // Pasa a canal 2
+        tv1.subirCanal(); // Pasa a canal 3
+        System.out.println("canal1: " + tv1.canal); // Imprime 3
+
+        tv1.bajarCanal(); // Pasa a canal 2
+        tv1.bajarCanal(); // Pasa a canal 1
+        tv1.bajarCanal(); // No baja más (límite 1)
+        System.out.println("canal1 tras bajar: " + tv1.canal); // Se corrige para imprimir tv1 en vez de tv2
+
+        System.out.println("\nDemostración de corrección de overflow:");
+        long a = 2000000000L, b = 2000000000L; // Se usa 'long' en lugar de 'int'
+        long c = a + b;
+        System.out.println("a + b = " + c); // Imprime 4000000000
     }
 }
-class TV{
-    public int canal;
+
+class TV {
+    public int canal = 1; // Inicializar en canal 1 por defecto
     public int volumen;
     public boolean on;
-    public void encender(){
-        on=true;
+
+    public void encender() {
+        on = true;
     }
-    public void apagar(){
-        on=false;
+
+    public void apagar() {
+        on = false;
     }
-    public void subirCanal(){
-        if(on && canal<120) canal++;
+
+    public void subirCanal() {
+        if (on && canal < 120) canal++;
     }
-    public void bajarCanal(){
-        if(on && canal>1) canal--;
+
+    public void bajarCanal() {
+        if (on && canal > 1) canal--;
     }
-    public void setCanal(int canal){
-        if(canal>=1 && canal<=120){
-            this.canal=canal;
+
+    public void setCanal(int canal) {
+        if (on && canal >= 1 && canal <= 120) {
+            this.canal = canal;
         }
     }
 }
